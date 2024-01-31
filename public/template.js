caeee70f272fc4847bb34e61c856d19cd31b2965
ZYJ <d2201501@sohu.com>
2024-02-01 01:39:52 +0800

    v0.0.6

--- public/template.js
+++ public/template.js
@@ -1,5 +1,5 @@
-let navigation_is_hide = true; // 左侧导航栏是否收起
-let options_is_hide = true; // 更多操作栏是否收起
+var navigation_is_hide = true; // 左侧导航栏是否收起
+var options_is_hide = true; // 更多操作栏是否收起
 var shapes = ["m-square1", "m-circle1", "m-square2", "m-circle2"]; // 鼠标点击特效样式表
 
 // window.addEventListener( 'resize', function() { // 监听窗口大小改变
@@ -56,16 +56,15 @@ document.getElementById("optionsListBar1").addEventListener('click', function() 
 });
 
 document.getElementById("optionsListBar2").addEventListener('click', function() { // 关于本站
-    console.log("关于本站")
+    console.log("about");
 });
 
 document.getElementById("navSearch").addEventListener('keyup', function(event) { // 导航栏-搜索框
     var input_content;
     if(event.key === 'Enter') { // 按下的是回车键
         input_content = document.getElementById("navSearch").value;
-        if(input_content == "help") { // 搜索帮助
-            raiseAlert("施工中", 1400);
-        }
+        if(input_content == "help") // 搜索帮助
+            createSearchHelp();
         else if(input_content == "#ma-on") { // 开启鼠标点击特效
             document.addEventListener("click", mouseAnimate);
             raiseAlert("鼠标点击特效已开启", 1400);
@@ -108,4 +107,61 @@ function raiseAlert(text, time){ // 开启一个提示框
     warnbox.innerHTML = text;
     document.body.appendChild(warnbox);
     setTimeout(destroy_alertBox, time);
+}
+
+function createSearchHelp(){ // 打开搜索指南
+    var help_mask = document.createElement("div");
+    var help_box = document.createElement("div");
+    var scrollfield = document.createElement("div");
+    var help_title = document.createElement("div");
+    help_mask.id = "searchHelpMask";
+    help_box.id = "searchHelpBox";
+    scrollfield.classList.add("help-box-scrollfield");
+    help_title.classList.add("help-box-title");
+    help_title.innerHTML = "※ 搜索指南 ※";
+    // 以下添加搜索指令 style: blue/orange/red/green/default
+    scrollfield.appendChild(help_title);
+    scrollfield.appendChild(createHelpList("help", "打开搜索指南"));
+    scrollfield.appendChild(createHelpList("#ma-on", "打开鼠标点击特效", "green"));
+    scrollfield.appendChild(createHelpList("#ma-off", "关闭鼠标点击特效", "red"));
+    // 以上添加搜索指令
+    help_mask.addEventListener("click", function() {
+        document.body.removeChild(document.getElementById("searchHelpBox"));
+        document.body.removeChild(document.getElementById("searchHelpMask"));
+    });
+    help_box.appendChild(scrollfield);
+    document.body.appendChild(help_mask);
+    document.body.appendChild(help_box);
+}
+
+function createHelpList(inst, illu, style){ // 搜索指令、介绍、样式选项
+    var help_list = document.createElement("div");
+    var help_inst = document.createElement("div");
+    var help_illu = document.createElement("div");
+    help_inst.innerHTML = inst;
+    help_illu.innerHTML = illu;
+    help_list.classList.add("help-box-list");
+    help_inst.classList.add("help-box-inst");
+    help_illu.classList.add("help-box-illu");
+    switch(style){
+        case "blue":
+            help_inst.style.border = "1px solid #76C1FF";
+            help_inst.style.backgroundColor = "#85EDFF";
+            break;
+        case "orange":
+            help_inst.style.border = "1px solid #FFDB4B";
+            help_inst.style.backgroundColor = "#FFF850";
+            break;
+        case "green":
+            help_inst.style.border = "1px solid #37C42F";
+            help_inst.style.backgroundColor = "#54D97F";
+            break;
+        case "red":
+            help_inst.style.border = "1px solid #FF7777";
+            help_inst.style.backgroundColor = "#FFA1A1";
+            break;
+    }
+    help_list.appendChild(help_inst);
+    help_list.appendChild(help_illu);
+    return help_list;
 }
